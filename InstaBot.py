0e32ca14a3629fa2b855e4f9f3aabcdc51722853
Sachin0987po <49241291+Sachin0987po@users.noreply.github.com>
2020-04-15 09:54:01 +0530

    Update InstaBot.py

--- InstaBot.py
+++ InstaBot.py
@@ -2,23 +2,24 @@ from selenium import webdriver
 from time import sleep
 
 class InstaBot:
+    
     def __init__(self, username, password):
         self.driver = webdriver.Firefox(executable_path='geckodriver.exe')
-        self.driver.get('https://www.instagram.com/')
+        self.driver.get('https://www.instagram.com/') # open instagram login page
         sleep(5)
-        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input').send_keys(username)
-        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input').send_keys(password)
-        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button').click()
+        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input').send_keys(username) # Enter username in username field
+        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input').send_keys(password) # Enter password in password fiels
+        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button').click() # click login button
         sleep(5)
         
 
     def get_following(self):
-        self.driver.get('https://www.instagram.com/sachinmangla843/') 
-        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/div/header/section/ul/li[3]/a').click()
+        self.driver.get('https://www.instagram.com/sachinmangla843/') # open user profile page
+        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/div/header/section/ul/li[3]/a').click() # click on following
         sleep(3)
-        scroll_box = self.driver.find_element_by_xpath("/html/body/div[4]/div/div[2]")
+        scroll_box = self.driver.find_element_by_xpath("/html/body/div[4]/div/div[2]") # scroll box
         last_ht, ht = 0, 1
-        while last_ht != ht:
+        while last_ht != ht: # scroll following list till end
             last_ht = ht
             sleep(5)
             ht = self.driver.execute_script("""
@@ -26,18 +27,17 @@ class InstaBot:
                 return arguments[0].scrollHeight;
                 """, scroll_box)
         links = scroll_box.find_elements_by_tag_name('a')
-        names = [name.text for name in links if name.text != '']
-        # close button
+        names = [name.text for name in links if name.text != ''] # name of user following
         self.driver.find_element_by_xpath("/html/body/div[4]/div/div[1]/div/div[2]/button").click()
         return names
 
     def get_follows(self):
-        self.driver.get('https://www.instagram.com/sachinmangla843/')
-        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/div/header/section/ul/li[2]/a').click()
+        self.driver.get('https://www.instagram.com/sachinmangla843/') # open user profile page again
+        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/div/header/section/ul/li[2]/a').click() # click on follows
         sleep(3)
-        scroll_box = self.driver.find_element_by_xpath("/html/body/div[4]/div/div[2]")
+        scroll_box = self.driver.find_element_by_xpath("/html/body/div[4]/div/div[2]") # scroll box
         last_ht, ht = 0, 1
-        while last_ht != ht:
+        while last_ht != ht:  # scroll follows list till end
             last_ht = ht
             sleep(5)
             ht = self.driver.execute_script("""
@@ -45,19 +45,25 @@ class InstaBot:
                 return arguments[0].scrollHeight;
                 """, scroll_box)
         links = scroll_box.find_elements_by_tag_name('a')
-        names = [name.text for name in links if name.text != '']
+        names = [name.text for name in links if name.text != ''] # name of user follows
         # close button
         self.driver.find_element_by_xpath("/html/body/div[4]/div/div[1]/div/div[2]/button").click()
         return names
 
-Username = 'Enter your username'
-Password = 'Enter you password'
-bot = InstaBot(Username, Password)
+    def close(self):
+        self.driver.close() # Close instapage
+
+username = 'Enter your username'
+password = 'Enter you password'
+
+bot = InstaBot(username, password)
 list1 = bot.get_following()
 list2 = bot.get_follows()
+bot.close()
 
+print('No of Following :- ', len(list1))
+print('No of Follows :- ',len(list2))
 
 for name in list1:
     if not name in list2:
-	print(name)
-        
+       print(name) 
